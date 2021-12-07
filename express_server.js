c80d26001d03d4d81949ad4084ce12f5298733aa
SaranyaSagi <saranya108sagi@gmail.com>
2021-12-07 13:36:29 -0500

    add post route that removes URL resource

--- express_server.js
+++ express_server.js
@@ -60,6 +60,18 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) =>{
+
+  // extract the id
+  const shortURL = req.params.shortURL;
+
+  // delete this joke from db
+  delete urlDatabase[shortURL];
+
+  res.redirect('/urls')
+
+})
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -68,9 +80,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
