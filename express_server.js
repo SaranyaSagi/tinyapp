9c58ce8e91e3dcfeebf43c090586b4358eba1db0
SaranyaSagi <saranya108sagi@gmail.com>
2021-12-06 20:29:15 -0500

    eslinted and fixed lint errors

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; //default port 8080
+const PORT = 8080;
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,21 +10,19 @@ const urlDatabase = {
 };
 
 const bodyParser = require("body-parser");
-const { response } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
+//const { response } = require("express");
 
-//for now return a string of 6 random alphanumeric characters
-function generateRandomString(length = 6) {
-//returns random string to be used as shortURL
-    let result  = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
-}
+const generateRandomString = function(length = 6) {
+//returns random (6 alphanumeric characters) string to be used as shortURL
+  let result  = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -37,6 +35,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//line 58 gets redirected here
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -46,20 +45,20 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
+  //storing the key value pair
   //obj['shortURL'] = value/longURL
-  urlDatabase[shortURL] = longURL
+  urlDatabase[shortURL] = longURL;
   
   console.log(req.body);
-  res.redirect(`/urls/${shortURL}`) 
-})
+  res.redirect(`/urls/${shortURL}`);
+});
 
+//going to that link
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
-})
-
-
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -72,7 +71,6 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
